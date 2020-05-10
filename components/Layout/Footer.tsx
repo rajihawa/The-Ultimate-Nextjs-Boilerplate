0111fb233efff77120fca6c0853866acef6e86e5
unknown <rajihawa@gmail.com>
2020-05-10 15:01:28 +0300

    Initial commit

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -4,32 +4,12 @@ import {
   Container,
   createStyles,
   makeStyles,
-  Theme,
   Typography,
-  IconButton
 } from "@material-ui/core";
-import PhoneIcon from "@material-ui/icons/Phone";
-import MailIcon from "@material-ui/icons/Mail";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import YouTubeIcon from "@material-ui/icons/YouTube";
-import { orange } from "@material-ui/core/colors";
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
-    root: {
-      width: "100%",
-      background: "#3d0081"
-    },
-    line: {
-      width: "100%",
-      paddingTop: theme.spacing(1),
-      marginBottom: theme.spacing(1),
-      borderBottom: "3px solid " + orange[400]
-    },
-    Icon: {
-      margin: theme.spacing(0, 1, 0, 0)
-    }
+    root: {},
   })
 );
 
@@ -39,66 +19,8 @@ const Footer: React.FC = () => {
   return (
     <Box className={classes.root}>
       <Container maxWidth="lg">
-        <Box className={classes.line} />
-        <Box
-          display="flex"
-          alignItems="center"
-          flexWrap="wrap"
-          justifyContent="space-around"
-        >
-          <Box display="flex" alignItems="center">
-            <PhoneIcon
-              className={classes.Icon}
-              color="secondary"
-              fontSize="large"
-            />
-            <Typography variant="h6" color="secondary">
-              054-6299985 / 072-2733993
-            </Typography>
-          </Box>
-          <Box display="flex" alignItems="center">
-            <a rel="noopener" href="mailto:info@edrsoft.com" target="_blank">
-              <IconButton aria-label="facebook link" component="span">
-                <MailIcon color="secondary" fontSize="large" />
-              </IconButton>
-            </a>
-
-            <Typography variant="h6" color="secondary">
-              info@edrsoft.com
-            </Typography>
-          </Box>
-          <Box display="flex" alignItems="center">
-            <a
-              rel="noopener"
-              href="https://www.facebook.com/profile.php?id=100018710410909"
-              target="_blank"
-            >
-              <IconButton aria-label="facebook link" component="span">
-                <FacebookIcon color="secondary" fontSize="large" />
-              </IconButton>
-            </a>
-            <a
-              rel="noopener"
-              href="https://www.linkedin.com/in/emil-hawa-4b2b6717/"
-              target="_blank"
-            >
-              <IconButton aria-label="linkedin link" component="span">
-                <LinkedInIcon color="secondary" fontSize="large" />
-              </IconButton>
-            </a>
-            <a
-              rel="noopener"
-              href="https://www.youtube.com/user/emilhawa2"
-              target="_blank"
-            >
-              <IconButton aria-label="youtube link" component="span">
-                <YouTubeIcon color="secondary" fontSize="large" />
-              </IconButton>
-            </a>
-          </Box>
-        </Box>
-        <Typography align="center" variant="body1" color="secondary">
-          © Copyright 2020 All Rights Reserved To EDR Software ltd.
+        <Typography align="center" variant="body1">
+          © Copyright 2020 All Rights Reserved To Website Owner.
         </Typography>
       </Container>
     </Box>
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Head from "next/head";
 import Navbar from "./Navbar";
 import { Box, makeStyles, Theme, createStyles } from "@material-ui/core";
+import Footer from "./Footer";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {},
-    body: {},
+    body: {
+      minHeight: "100vh",
+    },
     scrollTop: {
       position: "fixed",
       bottom: theme.spacing(2),
@@ -31,6 +34,7 @@ const Layout: React.FC<LayoutProps> = ({ title, children }) => {
       </Head>
       <Navbar />
       <Box className={classes.body}>{children}</Box>
+      <Footer />
     </Box>
   );
 };
